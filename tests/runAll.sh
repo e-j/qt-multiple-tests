d4eaa901bfc30410de5f8a0d2cf99af8763f4dd0
ejouffrey <eric.jouffrey@asygn.com>
2015-05-05 10:55:58 +0200

    Basic tests runner for several directories

--- tests/runAll.sh
+++ tests/runAll.sh
@@ -1,1 +1,45 @@
 #!/bin/bash
+
+testConfig(){
+    # Configure bash environment for test execution
+    set -ev # Verbose mode and exit script as soon as one command fail
+}
+
+projectClean(){
+    rm -f multiTestsCase
+}
+projectPrepare(){
+    qmake -project
+    echo "include(../base.pri)" >> *.pro
+    qmake
+}
+projectCompile(){
+    make
+}
+projectRun(){
+    ./multiTestsCase
+}
+
+
+proceedAllTests(){
+    for dir in ./*/
+    do
+        dir=${dir%*/}
+        echo "== Case "${dir##*/}" == "
+
+        cd $dir
+        projectClean
+        projectPrepare
+        projectCompile
+        projectRun
+        cd ..
+
+    done
+}
+
+main() {
+    testConfig
+    proceedAllTests
+}
+
+main
