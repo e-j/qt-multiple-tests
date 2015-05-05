6b2e78005589173f52d4a176aff126d244d747a2
ejouffrey <eric.jouffrey@asygn.com>
2015-05-05 16:06:29 +0200

    Add a verbose option on run all tests

--- tests/runAll.sh
+++ tests/runAll.sh
@@ -14,6 +14,55 @@ testConfig(){
     set -ev # Verbose mode and exit script as soon as one command fail
 }
 
+usage() {
+    echo "Script for run a bunch of test on qt-multiple-tests project"
+    echo "Each test is  a directory containing a Qt QTest simple project"
+    echo "That will be run"
+    echo "Usage : $PROGNAME [options]"
+    echo " "
+    echo "Options : "
+    echo "--verbose  -v : Output compilation and each of project runner"
+
+    echo " "
+    echo "--help     -h : Display this Help on usage"
+}
+
+cmdline() {
+    # got this idea from here:
+    # http://kirk.webfinish.com/2009/10/bash-shell-script-to-use-getopts-with-gnu-style-long-positional-parameters/
+    local arg=
+    for arg
+    do
+        local delim=""
+        case "$arg" in
+            #translate --gnu-long-options to -g (short options)
+            --help)           args="${args}-h ";;
+            --verbose)          args="${args}-v ";;
+            #pass through anything else
+            *) [[ "${arg:0:1}" == "-" ]] || delim="\""
+                args="${args}${delim}${arg}${delim} ";;
+        esac
+    done
+
+    #Reset the positional parameters to the short options
+    eval set -- $args
+
+    while getopts "hv" OPTION
+    do
+         case $OPTION in
+         h)
+             usage
+             exit 0
+             ;;
+         v)
+             readonly MODE_VERBOSE=1
+             ;;
+        esac
+    done
+
+    return 0
+}
+
 projectClean(){
     rm -f $BIN_RUNNER $RUNNER_OUTPUT
 }
@@ -23,11 +72,17 @@ projectPrepare(){
     qmake
 }
 projectCompile(){
-    make
+    if [[ -n $MODE_VERBOSE ]]; then
+        make
+    else
+        make 1> /dev/null
+    fi
 }
 projectRun(){
     ./$BIN_RUNNER &> $RUNNER_OUTPUT || true
-    cat $RUNNER_OUTPUT
+    if [[ -n $MODE_VERBOSE ]]; then
+        cat $RUNNER_OUTPUT
+    fi
     if [ -e "$SPECIFIC_SCRIPT" ]
     then
         echo "=> $SPECIFIC_SCRIPT is launched"
@@ -53,7 +108,9 @@ proceedAllTests(){
 }
 
 main() {
+    cmdline $ARGS
     testConfig
+
     proceedAllTests
 }
 
