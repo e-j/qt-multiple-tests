526093168c5d6bf2f7cf9b6efb469d79e5b01524
ejouffrey <eric.jouffrey@asygn.com>
2015-05-05 15:13:33 +0200

    Create a common result parsing script

---
+++ tests/resultParsing.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+scriptSuccess=0
+
+log(){
+    # Log a successfull step
+    # param1 : decription of step
+    echo "===> [OK]   $1"
+}
+
+error(){
+    # Log a failure step (and set scriptSuccess to failure status)
+    # param1 : decription of step
+    echo "===> [FAIL] $1"
+    scriptSuccess=1
+}
+
+testIntEqual(){
+    # Test that two integers are equals
+    # param1 : The expected value
+    # param2 : The value we get
+    # param3 : Description of test
+    if [ $1 -ne $2 ]
+    then
+        error "$3 : expected $1, get $2"
+        exit 1
+    else
+        log "$3 : (get $1)"
+    fi
+}
+
+parseLog(){
+    nbCaseStarted=`grep -o "Start testing of " output.log | wc -l`
+}
