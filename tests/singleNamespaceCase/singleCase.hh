296a7faa39460d93c64f3c4bd0bfc89c521c6d7a
ejouffrey <eric.jouffrey@gmail>
2016-10-20 15:12:09 +0200

    Test that namespace is added to test class name

---
+++ tests/singleNamespaceCase/singleCase.hh
@@ -0,0 +1,24 @@
+ #ifndef TESTDUMMY_HH_
+#define TESTDUMMY_HH_
+
+#include "../../MultiTests.hh"
+
+namespace test_space{
+
+class Dummy_test : public QObject{
+    Q_OBJECT
+
+    private slots:
+        // Test functions
+        void obviousTest(void){
+            QCOMPARE(1,1);
+        }
+};
+
+TEST_DECLARE(Dummy_test);
+
+}
+
+MULTI_TESTS_MAIN
+
+#endif /* TESTDUMMY_HH_ */
